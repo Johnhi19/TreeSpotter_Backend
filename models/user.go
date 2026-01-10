f2422f03b29726d38406d5bb4fcfa03cf75798ac
Johnhi19 <johannes.hierlinger@gmail.com>
2026-01-10 22:49:47 +0100

    User: Update model to contain email and timestamps

--- models/user.go
+++ models/user.go
@@ -1,7 +1,12 @@
 package models
 
+import "time"
+
 type User struct {
-	ID       int    `json:"user_id"`
-	Username string `json:"username"`
-	Password string `json:"password"`
+	ID        int       `json:"user_id"`
+	Username  string    `json:"username"`
+	Password  string    `json:"password"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+	ChangedAt time.Time `json:"changed_at"`
 }
